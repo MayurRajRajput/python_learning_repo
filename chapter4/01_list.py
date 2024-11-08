e575a76ae93ab21017fa0362e81f83edd329d200
MAYUR RAJ <133855647+MayurRajRajput@users.noreply.github.com>
2024-11-08 22:48:21 +0530

    Add files via upload

---
+++ chapter4/01_list.py
@@ -0,0 +1,8 @@
+friends=["mayur","apple",5,24.3,False,"praveen"]
+app = [2,3,4,2,6]
+app.sort()
+app.reverse()
+app.insert(2,"s")
+app.pop(2)
+app.remove(2)
+print(app)
---
+++ chapter4/practise_set.py
@@ -0,0 +1,23 @@
+# fruit=[]
+# a1 = input("enter the fruits :")
+# fruit.append(a1)
+# a2 = input("enter the fruits :")
+# fruit.append(a2)
+# a3 = input("enter the fruits :")
+# fruit.append(a3)
+# a4 = input("enter the fruits :")
+# fruit.append(a4)
+# a5 = input("enter the fruits :")
+# fruit.append(a5)
+# print(fruit)
+
+# for i in range(5):
+#     a = input("enter the fruits:")
+#     fruit.append(a)
+# print(fruit)    
+# a=(34,"mayur")
+# a[2]="this is"
+# number = [1,2,3,4]
+# print(sum(number))
+a = [1,2,3,0,0,3,4,0]
+print(a.count(0))
