9b7c092d0bca713b495233115c2bf8883d310aad
MAYUR RAJ <133855647+MayurRajRajput@users.noreply.github.com>
2024-11-10 22:17:36 +0530

    Add files via upload

---
+++ chapter6/practiseset.py
@@ -0,0 +1,65 @@
+# a1=int(input("enter the number1:"))
+# a2=int(input("enter the number1:"))
+# a3=int(input("enter the number1:"))
+# a4=int(input("enter the number1:"))
+
+# if(a1>a2 and a1>a3 and a1>a4):
+#     print(f"{a1} is greater")
+# elif(a2>a1 and a2>a3 and a2>a4):
+#     print(f"{a2} is greater")
+# elif(a3>a1 and a3>a2 and a3>a4):
+#     print(f"{a3} is greater")
+# else:
+#     print(f"{a4} is greater")
+
+# marks1=int(input("enter the number1:"))
+# marks2=int(input("enter the number2:"))
+# marks3=int(input("enter the number3:"))
+# sum=(marks1+marks2+marks3)
+# total_percentage=(sum/300)*100
+
+# if(total_percentage>=40 and marks1>=33 and marks2>=33 and marks3>=33):
+#     print("you r pass")
+# else:
+#     print("come next year")        
+
+# p1="make a money lot"
+# p2="spam mashg"
+# p3="spam1"
+# message=input("enter you comment: ")
+# if((p1 in message)or(p2 in message)or(p3 in message)):
+#     print("your r spamming me")
+# else:
+#     print("thank you for your comment")    
+# username = input("enter your username")
+# if(len(username)>=10):
+#     print("username is correct")
+# else:
+#     print("username is not valid")  
+
+# list = ["mayur","mohit","praveen","ajeevin"]
+# name = input("enter the name:")
+# if(name in list ):
+#     print(f"{name} is in list")
+# else:
+#     print(f"{name} is not in list")   
+
+# marks = int(input("enter your marks: "))
+# if(marks>=90 and marks<=100):
+#     print("Ex")
+# elif(marks>=80 and marks<90):
+#     print("A")   
+# elif(marks>=70 and marks<80):
+#     print("B")   
+# elif(marks>=60 and marks<70):
+#     print("C")   
+# elif(marks>=50 and marks<60):
+#     print("D")   
+# elif(marks<50 and marks>=0):
+#     print("F")  
+# else:
+#     print("entered marks is invalid") 
+
+post = input("enter the post")
+if("mayur" in post.lower()):
+    print("this is talking about mayur")   
