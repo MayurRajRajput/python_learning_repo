d3573b1496a0fbe20eac574939e6aaba561f076a
MAYUR RAJ <133855647+MayurRajRajput@users.noreply.github.com>
2024-11-25 22:57:37 +0530

    Add files via upload

---
+++ map&lambda/squaring_list.py
@@ -0,0 +1,5 @@
+# square in list 
+
+list1=[1,2,3,4]
+result=map(lambda x:x*x,list1)
+print(list(result))
---
+++ map&lambda/squaringlist.py
@@ -0,0 +1,5 @@
+# squaring with 2 in given list
+
+a=[1,2,3,4,5]
+res=list(map(lambda x:x*2,a))
+print(res)
