193935f77522e943c2c525362eb003bda8b409f0
MAYUR RAJ <133855647+MayurRajRajput@users.noreply.github.com>
2024-11-30 23:53:55 +0530

    Add files via upload

---
+++ numpy/basic_array_operations.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print("Original Array:",arr)
+print("Array +2: ",arr+2)
+print("Array *3: ",arr*3)
+print("square of the array: ",arr**2)
+# aggregate functions
+print("sum: ",np.sum(arr))
+print("Mean: ",np.mean(arr))
+print("Median: ",np.median(arr))
+print("Maximum: ",np.max(arr))
